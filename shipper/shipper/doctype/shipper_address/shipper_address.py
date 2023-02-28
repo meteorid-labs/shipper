207b72adbad9d1c01662789e0166e5addf2bb5d0
Aslam <aslam.hafidz@meteor.id>
2023-02-28 15:45:29 +0700

    Apply style using autopep8

--- shipper/shipper/doctype/shipper_address/shipper_address.py
+++ shipper/shipper/doctype/shipper_address/shipper_address.py
@@ -6,68 +6,72 @@ from frappe.model.document import Document
 import json
 from shipper.shipper.shipper import ShipperUtils
 
+
 class ShipperAddress(Document):
-	def validate(self):
-		try:
-			suburb = json.loads(self.suburb)
-			self.suburb = suburb.get('display_txt')
-		except ValueError:
-			pass
+    def validate(self):
+        try:
+            suburb = json.loads(self.suburb)
+            self.suburb = suburb.get('display_txt')
+        except ValueError:
+            pass
 
-		area = json.loads(self.area)
+        area = json.loads(self.area)
 
-		self.area = area.get('name')
-		self.meta_address = json.dumps(area)
-		self.postcode = area.get('postcode')
+        self.area = area.get('name')
+        self.meta_address = json.dumps(area)
+        self.postcode = area.get('postcode')
+
 
 @frappe.whitelist()
 def get_address_details(address):
-	return frappe.get_doc("Shipper Address", address)
+    return frappe.get_doc("Shipper Address", address)
+
 
 @frappe.whitelist()
 # @frappe.validate_and_sanitize_search_inputs
 def get_location_projection(txt, level, limit, sort_by):
-	locations = []
+    locations = []
 
-	response = ShipperUtils().location(params={
-			'keyword': txt,
-			'adm_level': level,
-			'limit': limit,
-			'sort_by': sort_by
-		})
+    response = ShipperUtils().location(params={
+        'keyword': txt,
+        'adm_level': level,
+        'limit': limit,
+        'sort_by': sort_by
+    })
 
-	result = frappe._dict(response.json())
+    result = frappe._dict(response.json())
 
-	if result.metadata.get('http_status_code') == 200:
-		for location in result.data:
-			locations.append({
-				'label': location.get('display_txt'),
-				'value': json.dumps(location),
-			})
+    if result.metadata.get('http_status_code') == 200:
+        for location in result.data:
+            locations.append({
+                'label': location.get('display_txt'),
+                'value': json.dumps(location),
+            })
 
-	return locations
+    return locations
+
 
 @frappe.whitelist()
 def get_areas(suburb_id, limit, sort_by):
-	areas = []
+    areas = []
 
-	params = {
-		limit: limit,
-		sort_by: sort_by
-	}
+    params = {
+        limit: limit,
+        sort_by: sort_by
+    }
 
-	if (suburb_id):
-		params['suburb_id'] = suburb_id
+    if (suburb_id):
+        params['suburb_id'] = suburb_id
 
-	response = ShipperUtils().areas(params=params)
+    response = ShipperUtils().areas(params=params)
 
-	result = frappe._dict(response.json())
+    result = frappe._dict(response.json())
 
-	if result.metadata.get('http_status_code') == 200:
-		for area in result.data:
-			areas.append({
-				'label': area.get('name'),
-				'value': json.dumps(area),
-			})
+    if result.metadata.get('http_status_code') == 200:
+        for area in result.data:
+            areas.append({
+                'label': area.get('name'),
+                'value': json.dumps(area),
+            })
 
-	return areas
+    return areas
--- shipper/shipper/doctype/shipper_settings/shipper_settings.js
+++ shipper/shipper/doctype/shipper_settings/shipper_settings.js
@@ -2,7 +2,6 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Shipper Settings', {
-	// refresh: function(frm) {
-
-	// }
-});
+  // refresh: function(frm) {
+  // }
+})
